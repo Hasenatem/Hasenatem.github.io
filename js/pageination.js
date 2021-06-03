644e5f1340dcbc831d62f06985419ca12f1fdbd4
LukasTinnes <49793331+LukasTinnes@users.noreply.github.com>
2021-06-03 13:03:02 +0200

    Update pageination.js

--- js/pageination.js
+++ js/pageination.js
@@ -1,9 +1,20 @@
+var list = new Array();
 $('.sync-pagination').twbsPagination({
         totalPages: 10,
         visiblePages: 4,
         next: 'Next',
         prev: 'Prev',
         onPageClick: function (evt, page) {
-            $('#content').text('Page ' + page);
+            loadpage();
+            $('#content').text('Page ' + list[page]);
         }
     });
+
+function loadpage() {
+    $.getJSON('../resources/pageination_example.json', function (data) {
+        list = data.content;
+    }).error(function(){
+            console.log('error: json not loaded');
+        });
+    });
+}
