ca1dedad08ea2f23af7fecad815fab3080ab9f0d
LukasTinnes <49793331+LukasTinnes@users.noreply.github.com>
2021-06-03 12:55:38 +0200

    del dynamic imports in lorem

--- js/pageination.js
+++ js/pageination.js
@@ -1,16 +1,3 @@
-
-
-let css = document.createElement("link");
-css.setAttribute("src", "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.min.css");
-css.setAttribute("async", "false");
-
-let jquery = document.createElement("script");
-jquery.setAttribute("src", "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js");
-jquery.setAttribute("async", "false");
-
-let bootstrap = document.createElement("script");
-bootstrap.setAttribute("src", "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/js/bootstrap.min.js");
-bootstrap.setAttribute("async
 $('.sync-pagination').twbsPagination({
         totalPages: 10,
         visiblePages: 4,
